302ffc0a0cec1dab979ee31e3da0e22e8646c6d7
efrat8106 <efrat2899@gmail.com>
2026-03-15 01:53:50 +0200

    Add Header component for Task Dashboard

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="p-4 bg-gray-900 text-white text-xl">
+      Task Dashboard
+    </header>
+  );
+}
+
+export default Header;
