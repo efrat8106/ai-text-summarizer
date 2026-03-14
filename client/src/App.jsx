9bc06c7dc9b0246fd494add4db51fc75cc79617b
efrat8106 <efrat2899@gmail.com>
2026-03-15 01:55:57 +0200

    Create App component with Header, Dashboard, and Settings

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+
+function App() {
+  return (
+    <div>
+
+      <Header />
+
+      <Dashboard />
+
+      <Settings />
+
+    </div>
+  );
+}
+
+export default App;
