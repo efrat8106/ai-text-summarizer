e254652fe0332e59c3291e0f7fd03f05a3074110
efrat8106 <efrat2899@gmail.com>
2026-03-15 01:55:17 +0200

    Add Settings page component

---
+++ client/src/pages/Settings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Settings() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold">Settings Page</h2>
+      <p>This is a secondary screen in the system.</p>
+    </div>
+  );
+}
+
+export default Settings;
