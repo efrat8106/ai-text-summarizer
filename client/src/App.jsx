896a8efac5b6fb1b26e9943d1402cb7b731ccd6a
efrat <efrat2899@gmail.com>
2026-03-19 02:10:14 +0200

    Fix App.jsx to include Router and Sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
 
 function App() {
   return (
-    <div>
-
-      <Header />
-
-      <Dashboard />
-
-      <Settings />
-
-    </div>
+    <Router>
+      <div className="flex flex-col h-screen overflow-hidden">
+        <Header />
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar />
+          <main className="flex-1 overflow-y-auto bg-gray-50 p-4">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
   );
 }
 
